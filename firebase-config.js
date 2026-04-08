db10171975c2adbf91c1b82519e88f4e8e114f12
muou-0208 <chenjianbing108@gmail.com>
2026-04-08 17:02:47 +0800

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization script.

--- firebase-config.js
+++ firebase-config.js
@@ -17,3 +17,26 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAN4PYv1nrjtkN85OwY4n4VjjBsQ90PPRI",
+    authDomain: "cly-0208.firebaseapp.com",
+    projectId: "cly-0208",
+    storageBucket: "cly-0208.firebasestorage.app",
+    messagingSenderId: "350193296343",
+    appId: "1:350193296343:web:f505379fcc24d204c0e192",
+    measurementId: "G-TCJNZGQ7WN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
